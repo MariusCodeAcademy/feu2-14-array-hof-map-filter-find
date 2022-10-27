fcf56824ba00c7aa37fdf7c97e144b569c738dac
Marius <marius@example.com>
2022-10-27 12:01:26 +0300

    filter

--- js/postsCreateFilter.js
+++ js/postsCreateFilter.js
@@ -21,8 +21,34 @@ const postsData = [
 // taikomes
 const list1El = document.getElementById('list1');
 
-const render = () => {
+const render = (arrOfPosts) => {
+  // issivalyti generuojama konteineri
+  list1El.innerHTML = '';
   console.log('render');
+  arrOfPosts.forEach((pObj) => {
+    // sukuriu elementa
+    const newPostEl = createPostLi(pObj);
+    // patalpinu elementa dom
+    list1El.append(newPostEl);
+  });
 };
 
-const createPostLi = () => {};
+const createPostLi = (postObj) => {
+  const liEl = document.createElement('li');
+  liEl.className = 'card';
+  liEl.innerHTML = `
+  <h3>${postObj.title}</h3>
+  <h4>${postObj.author}</h4>
+  <p><i>${postObj.year}</i></p>
+  `;
+  return liEl;
+};
+// const crEl = createPostLi(postsData[2]);
+// console.log('crEl ===', crEl);
+// list1El.append(crEl);
+
+// 1. pasiimti reiksme is input
+// 2. su reiksme filtruojam postsData pagal title (.includes)
+// 3. is isfiltruoto masyvo generuojam postus su render()
+
+render(postsData);
